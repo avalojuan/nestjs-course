203ef97c8210005890605bb8909bb8f357e88605
JuanAvalo <avalojuanma@gmail.com>
2022-06-10 18:31:43 -0300

    Added user CRUD

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +10,19 @@ export class User {
   
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert(){
+    console.log('inserted user with id ', this.id)
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id ', this.id)
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user witrh id ' , this.id)
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,35 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
+import { BADFAMILY } from 'dns';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
-  createUser() {
-    
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+  
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
